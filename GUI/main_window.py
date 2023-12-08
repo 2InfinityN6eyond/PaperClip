9b999090d7a28a27169f585e6470eee01a3393bc
taehpark0801 <taehrsproject@gmail.com>
2023-12-09 01:11:28 +0800

    main_window comment

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -5,11 +5,13 @@ from scrap_viewer import ScrapViewer
 from paper_gui import PaperGUI
 from center_section import CenterSection
 
+# Define Main GUI
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(
             self,
             query_handler
         ) :
+        # give access to properties of a parent & set window
         super().__init__()
         self.setGeometry(100, 100, 1200, 800)
         self.setWindowTitle('PaperClip')
@@ -18,11 +20,13 @@ class MainWindow(QtWidgets.QMainWindow):
         self.query_handler = query_handler
         self.popular_papers_window = None
 
+        # Define Splitter of Frame
         splitter = QtWidgets.QSplitter(self)
         splitter.setStyleSheet("background-color: #303030;")
         splitter.setStyleSheet("QSplitter::handle{background: white;}")
         splitter.setHandleWidth(1)
         
+        # Define 3 section, ScrapViewer, CenterSection, PaperGUI
         self.scraps_section = ScrapViewer(parent=self)
         self.scraps_section.update(
             self.query_handler.queryPaperBy(
@@ -38,11 +42,12 @@ class MainWindow(QtWidgets.QMainWindow):
             paper  = None
         )
 
-
+        # Add 3 section to frame
         splitter.addWidget(self.scraps_section)
         splitter.addWidget(self.center_section)
         splitter.addWidget(self.paper_section)
 
+        # set outermost layout and put sub-frame in it
         layout = QtWidgets.QVBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(splitter)
@@ -50,10 +55,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.central_window.setLayout(layout)
         self.setCentralWidget(self.central_window)
 
-
+    # Update papersection when paper item Clicked
     def paperItemClicked(self, item) :
         self.paper_section.update(item)
 
+    # Update all favorite lists that can be seen on the screen
     def favorite_list_changed(self, item):
         if item.authors is None :
             return
